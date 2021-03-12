7ab572e2dd646539f8cae7c55dac9b17d18a3361
Vitor Souza <vhsouzaeng@gmail.com>
2021-03-12 09:55:35 -0300

    chore: service client created

---
+++ src/services/impl/github.tsx
@@ -0,0 +1,29 @@
+import axios, { AxiosInstance } from "axios";
+import Service from "../services";
+
+interface Request {
+    url: string;
+}
+
+interface Response {
+    data: any;
+    status: number;
+}
+
+class GithubClient implements Service {
+    client: AxiosInstance;
+    constructor() {
+        this.client = axios.create({
+            baseURL: 'https://api.github.com/users',
+        });
+    }
+
+    async GET({ url }: Request): Promise<Response> {
+        const { data, status } = await this.client.get(url);
+        return {
+            data,
+            status
+        };
+    }
+
+}
--- src/services/services.tsx
+++ src/services/services.tsx
@@ -1,3 +1,13 @@
-interface Service{
-    
+interface Request {
+    url: string;
 }
+
+interface Response {
+    data: any;
+    status: number;
+}
+interface Service {
+    GET({ url }: Request): Promise<Response>;
+}
+
+export default Service;
